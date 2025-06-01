404c78fb3f5fa3bd0208055248fbe1c86ef08c13
Suhel1239 <124263113+Suhel1239@users.noreply.github.com>
2025-06-01 13:09:12 +0530

    Create glip_coco.py

---
+++ mmdet/datasets/glip_coco.py
@@ -0,0 +1,10 @@
+from mmdet.datasets import CocoDataset
+from mmengine.registry import DATASETS
+
+@DATASETS.register_module()
+class GLIPCocoDataset(CocoDataset):
+    def prepare_data(self, idx):
+        data = super().prepare_data(idx)
+        data['text'] = '. '.join(self.CLASSES)
+        data['custom_entities'] = list(self.CLASSES)
+        return data
