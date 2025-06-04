abe5b2dbc9e8c5fd408d7905b6137af103c28749
Suhel1239 <124263113+Suhel1239@users.noreply.github.com>
2025-06-04 16:01:49 +0530

    Update inference.py

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -23,6 +23,122 @@ from ..structures import DetDataSample, SampleList
 from ..utils import get_test_pipeline_cfg
 
 
+# def init_detector(
+#     config: Union[str, Path, Config],
+#     checkpoint: Optional[str] = None,
+#     palette: str = 'none',
+#     device: str = 'cuda:0',
+#     cfg_options: Optional[dict] = None,
+# ) -> nn.Module:
+#     """Initialize a detector from config file.
+
+#     Args:
+#         config (str, :obj:`Path`, or :obj:`mmengine.Config`): Config file path,
+#             :obj:`Path`, or the config object.
+#         checkpoint (str, optional): Checkpoint path. If left as None, the model
+#             will not load any weights.
+#         palette (str): Color palette used for visualization. If palette
+#             is stored in checkpoint, use checkpoint's palette first, otherwise
+#             use externally passed palette. Currently, supports 'coco', 'voc',
+#             'citys' and 'random'. Defaults to none.
+#         device (str): The device where the anchors will be put on.
+#             Defaults to cuda:0.
+#         cfg_options (dict, optional): Options to override some settings in
+#             the used config.
+
+#     Returns:
+#         nn.Module: The constructed detector.
+#     """
+#     if isinstance(config, (str, Path)):
+#         config = Config.fromfile(config)
+#     elif not isinstance(config, Config):
+#         raise TypeError('config must be a filename or Config object, '
+#                         f'but got {type(config)}')
+#     if cfg_options is not None:
+#         config.merge_from_dict(cfg_options)
+#     elif 'init_cfg' in config.model.backbone:
+#         config.model.backbone.init_cfg = None
+
+#     scope = config.get('default_scope', 'mmdet')
+#     if scope is not None:
+#         init_default_scope(config.get('default_scope', 'mmdet'))
+
+#     model = MODELS.build(config.model)
+#     model = revert_sync_batchnorm(model)
+#     if checkpoint is None:
+#         warnings.simplefilter('once')
+#         warnings.warn('checkpoint is None, use COCO classes by default.')
+#         model.dataset_meta = {'classes': get_classes('coco')}
+#     else:
+#         checkpoint = load_checkpoint(model, checkpoint, map_location='cpu')
+#         # Weights converted from elsewhere may not have meta fields.
+#         checkpoint_meta = checkpoint.get('meta', {})
+
+#         # save the dataset_meta in the model for convenience
+#         if 'dataset_meta' in checkpoint_meta:
+#             # mmdet 3.x, all keys should be lowercase
+#             model.dataset_meta = {
+#                 k.lower(): v
+#                 for k, v in checkpoint_meta['dataset_meta'].items()
+#             }
+#         elif 'CLASSES' in checkpoint_meta:
+#             # < mmdet 3.x
+#             classes = checkpoint_meta['CLASSES']
+#             model.dataset_meta = {'classes': classes}
+#         else:
+#             warnings.simplefilter('once')
+#             warnings.warn(
+#                 'dataset_meta or class names are not saved in the '
+#                 'checkpoint\'s meta data, use COCO classes by default.')
+#             model.dataset_meta = {'classes': get_classes('coco')}
+
+#     # Priority:  args.palette -> config -> checkpoint
+#     if palette != 'none':
+#         model.dataset_meta['palette'] = palette
+#     else:
+#         test_dataset_cfg = copy.deepcopy(config.test_dataloader.dataset)
+#         # lazy init. We only need the metainfo.
+#         test_dataset_cfg['lazy_init'] = True
+#         metainfo = DATASETS.build(test_dataset_cfg).metainfo
+#         cfg_palette = metainfo.get('palette', None)
+#         if cfg_palette is not None:
+#             model.dataset_meta['palette'] = cfg_palette
+#         else:
+#             if 'palette' not in model.dataset_meta:
+#                 warnings.warn(
+#                     'palette does not exist, random is used by default. '
+#                     'You can also set the palette to customize.')
+#                 model.dataset_meta['palette'] = 'random'
+
+#     model.cfg = config  # save the config in the model for convenience
+#     model.to(device)
+#     model.eval()
+#     return model
+
+#################################################################################
+import warnings
+import copy
+from pathlib import Path
+from typing import Union, Optional, Sequence
+
+import torch
+import numpy as np
+from torch import nn
+from mmengine.config import Config
+from mmengine.utils import revert_sync_batchnorm
+from mmengine.registry import DATASETS, MODELS
+from mmengine.runner import load_checkpoint  # We'll avoid using this for loading weights here
+
+from mmengine.utils import get_classes
+from mmengine.hooks import init_default_scope
+
+# For patching safe globals during torch.load
+from torch.serialization import add_safe_globals
+from mmengine.logging import HistoryBuffer
+from numpy.core.multiarray import _reconstruct
+from numpy import dtype
+
+
 def init_detector(
     config: Union[str, Path, Config],
     checkpoint: Optional[str] = None,
@@ -65,14 +181,23 @@ def init_detector(
 
     model = MODELS.build(config.model)
     model = revert_sync_batchnorm(model)
+
+    # Patch safe globals before loading checkpoint weights
+    add_safe_globals([HistoryBuffer, _reconstruct, dtype])
+
     if checkpoint is None:
         warnings.simplefilter('once')
         warnings.warn('checkpoint is None, use COCO classes by default.')
         model.dataset_meta = {'classes': get_classes('coco')}
     else:
-        checkpoint = load_checkpoint(model, checkpoint, map_location='cpu')
-        # Weights converted from elsewhere may not have meta fields.
-        checkpoint_meta = checkpoint.get('meta', {})
+        # Manually load checkpoint with patched safe globals
+        checkpoint_data = torch.load(checkpoint, map_location='cpu')
+
+        # Extract state_dict or fallback to entire checkpoint_data
+        state_dict = checkpoint_data.get('state_dict', checkpoint_data)
+        model.load_state_dict(state_dict, strict=False)
+
+        checkpoint_meta = checkpoint_data.get('meta', {})
 
         # save the dataset_meta in the model for convenience
         if 'dataset_meta' in checkpoint_meta:
@@ -114,6 +239,7 @@ def init_detector(
     model.to(device)
     model.eval()
     return model
+################################################################################
 
 
 ImagesType = Union[str, np.ndarray, Sequence[str], Sequence[np.ndarray]]
